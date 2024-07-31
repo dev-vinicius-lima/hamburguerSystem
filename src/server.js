f23ae7e78dd594b7f1ab7cde24f682ea02ad269c
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-07-30 20:38:12 -0400

    feat: :sparkles: create migration for table users

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
-import app from "../src/app.js";
+import app from '../src/app.js'
+import connection from '../src/config/database.js'
+
+connection
+	.authenticate()
+	.then(() => {
+		console.log('Connection has been established successfully.')
+	})
+	.catch((err) => {
+		console.error('Unable to connect to the database:', err)
+	})
 
 app.listen(3333, () => {
-  console.log("Server is running on port 3333");
-});
+	console.log('Server is running on port 3333')
+})
