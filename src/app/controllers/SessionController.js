912915c8a8ddc539ce0b70f160f34bbc7d6e58ca
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-07-31 14:16:42 -0400

    feat: :sparkles: create SessionController with validation in Yup

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,46 @@
+import * as Yup from 'yup'
+import User from '../models/User.js'
+
+class SessionController {
+	async store(req, res) {
+		const schema = Yup.object().shape({
+			email: Yup.string().email().required(),
+			password: Yup.string().required(),
+		})
+
+		try {
+			await schema.validate(req.body)
+		} catch (error) {
+			return res
+				.status(400)
+				.json({ message: 'Login failed sure email or password are incorrect' })
+		}
+
+		const { email, password } = req.body
+
+		const user = await User.findOne({ where: { email } })
+
+		if (!user) {
+			return res
+				.status(404)
+				.json({ message: 'Login failed sure email or password are incorrect' })
+		}
+
+		const passwordMatch = await user.checkPassword(password)
+
+		if (!passwordMatch) {
+			return res
+				.status(401)
+				.json({ message: 'Login failed sure email or password are incorrect' })
+		}
+
+		return res.json({
+			id: user.id,
+			email,
+			name: user.name,
+			admin: user.admin,
+		})
+	}
+}
+
+export default new SessionController()
