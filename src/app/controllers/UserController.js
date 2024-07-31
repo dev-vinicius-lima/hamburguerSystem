143094df8416f2bcd030b6cb38da3304b9e9c10b
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-07-31 13:18:31 -0400

    feat: :sparkles: add validation with yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,20 @@
 import User from '../models/User.js'
-
+import * as Yup from 'yup'
 class UserController {
 	async store(req, res) {
+		const schema = Yup.object().shape({
+			name: Yup.string().required(),
+			email: Yup.string().email().required(),
+			password_hash: Yup.string().required().min(6),
+			admin: Yup.boolean(),
+		})
+
+		try {
+			await schema.validateSync(req.body, { abortEarly: false })
+		} catch (error) {
+			return res.status(400).json({ message: error.errors })
+		}
+
 		const { name, email, password_hash, admin } = req.body
 		try {
 			const user = await User.create({ name, email, password_hash, admin })
