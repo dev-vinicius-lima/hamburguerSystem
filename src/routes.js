7ccc2e984657aa6a228fdc60959f85bd11af05a9
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-07-31 14:16:11 -0400

    feat: :sparkles: create router session

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import UserController from './app/controllers/UserController.js'
+import SessionController from './app/controllers/SessionController.js'
 const routes = Router()
 
 routes.post('/users', UserController.store)
+routes.post('/sessions', SessionController.store)
 
 export default routes
