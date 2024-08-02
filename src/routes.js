f5cfb83ad76c9bbbdcd3a34c4173959053741ddd
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-08-01 20:25:23 -0400

    feat: :sparkles: create routes post and get for /categories

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,18 @@ import multerConfig from './config/multer.cjs'
 import UserController from './app/controllers/UserController.js'
 import SessionController from './app/controllers/SessionController.js'
 import ProductController from './app/controllers/ProductController.js'
+import CategoryController from './app/controllers/CategoryController.js'
 import authMiddleware from './app/middlewares/auth.js'
 const routes = Router()
 const upload = multer(multerConfig)
 
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
+
 routes.use(authMiddleware)
 routes.post('/products', upload.single('file'), ProductController.store)
 routes.get('/products', ProductController.index)
+routes.post('/categories', CategoryController.store)
+routes.get('/categories', CategoryController.index)
 
 export default routes
