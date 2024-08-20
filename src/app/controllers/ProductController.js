2416449a68045361b69776bfd0716892c4c1eb9c
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-08-20 16:13:38 -0400

    feat: :sparkles: adjust logic the ProductController

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -8,7 +8,7 @@ class ProductController {
 			name: Yup.string().required(),
 			price: Yup.number().required(),
 			category_id: Yup.number().required(),
-			offer: Yup.boolean(),
+			offer: Yup.boolean().optional(),
 		})
 
 		try {
@@ -22,8 +22,18 @@ class ProductController {
 			return res.status(401).json()
 		}
 
+		if (!req.file) {
+			return res.status(400).json({ message: 'File not found please upload' })
+		}
 		const { filename: path } = req.file
+		if (!path) {
+			return res.status(400).json({ message: 'File not found' })
+		}
 		const { name, price, category_id, offer } = req.body
+		const isAllFieldsFilled = Boolean(name && price && category_id)
+		if (!isAllFieldsFilled) {
+			return res.status(400).json({ message: 'All fields are required' })
+		}
 
 		const product = await Product.create({
 			name,
@@ -59,28 +69,26 @@ class ProductController {
 
 		const { admin: isAdmin } = await User.findByPk(req.userId)
 		if (!isAdmin) {
-			return res.status(401).json()
+			return res.status(401).json({ message: 'Unauthorized' })
 		}
 
 		const { id } = req.params
 		const product = await Product.findByPk(id)
 		if (!product) {
-			return res.status(401).json({ message: 'Id the Product not found' })
-		}
-
-		let path
-		if (req.file) {
-			path = req.file.filename
+			return res.status(404).json({ message: 'Product not found' })
 		}
 
 		const { name, price, category_id, offer } = req.body
+
+		if (!name && !price && !category_id && !offer) {
+			return res.status(400).json({ messages: 'All fields are required' })
+		}
 
 		await Product.update(
 			{
 				name,
 				price,
 				category_id,
-				path,
 				offer,
 			},
 			{
